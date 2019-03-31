6c3d793e25257f7c5adf2064b76c6472a80a4091
iqoOopi <46897552+iqoOopi@users.noreply.github.com>
2019-03-30 22:58:13 -0600

    genericDB added

---
+++ CustomerDataLayer/TravelExpertDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDB
+{
+    public static class TravelExpertDB
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString = @"Data Source=localhost\Sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
