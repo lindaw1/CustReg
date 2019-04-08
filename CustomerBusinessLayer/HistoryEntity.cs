a7310e0642fdd4d49daba4e2299ec26a008c7717
Hoorasadat Mirsanei <mirsaanei@gmail.com>
2019-04-08 12:49:39 -0600

    headers' name

--- CustomerBusinessLayer/HistoryEntity.cs
+++ CustomerBusinessLayer/HistoryEntity.cs
@@ -15,20 +15,16 @@ namespace CustomerBusinessLayer
 
         public int? BookingId { get; set; }
         
-        [DisplayName("Package ID")]
         public int? PackageId { get; set; }
 
-        [DisplayName("Package Name")]
         public string PackageName { get; set; }
 
-        [DisplayName("# of Travelers")]
         public float? TravelerCount { get; set; }
 
         public decimal BasePrice { get; set; }
 
         public decimal AgencyCom { get; set; }
 
-        [DisplayName("Total Cost")]
         public decimal Total { get; set; }
     }
 }
